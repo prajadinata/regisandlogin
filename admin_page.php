2cc1323199e7eae70d1f88edb46e010c703a3d0f
adi p <adiprajadinata3@gmail.com>
2025-08-08 19:32:11 +0700

    Create admin_page.php

---
+++ admin_page.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>regis and login</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+  </head>
+  <body>
+    <div class="container">
+      <div class="content">
+        <h3>Hi, <span>Admin</span></h3>
+        <h1>Welcome! <span></span></h1>
+        <p>This is an admin page</p>
+        <a href="login_form.php" class="btn">Login</a>
+        <a href="register_form.php" class="btn">Register</a>
+        <a href="logout.php" class="btn">Logout</a>
+      </div>
+    </div>
+  </body>
+</html>
