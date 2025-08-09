1468b5c0b3eb22d901ec509f67ff32e6ee6dd51d
adi p <adiprajadinata3@gmail.com>
2025-08-09 17:12:26 +0700

    Create index.php
    add a form.

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>login and register</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  </head>
+  <body>
+    <div class="container">
+      <div class="content">
+        <form action="#">
+          <h2>Login</h2>
+          <label for="username">Username:</label>
+          <input type="text" name="username" placeholder="Input here" required>
+          
+          <label for="password">Password:</label>
+          <input type="password" placeholder="Input here" required>
+          <hr>
+          <button type="submit" name="login">Login</button>
+          <p>Don't have an aacount? <a href="#">Register</a></p>
+        </form>
+      </div>
+    </div>
+  </body>
+</html>
