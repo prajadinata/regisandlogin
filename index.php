e39467b12605765a50df86c811523e414c55bbe3
adi p <adiprajadinata3@gmail.com>
2025-08-11 16:02:42 +0700

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <title>login and register</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style
+    <link rel="stylesheet" href="style">
   </head>
   <body>
     <div class="container">
